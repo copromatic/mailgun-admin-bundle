aef23cb49365c99989e35a936f8d8efa5f7c5d4a
Yannis <gowtcha@gmail.com>
2017-10-02 17:30:53 +0200

    add import controller and request

--- Controller/WebhookController.php
+++ Controller/WebhookController.php
@@ -2,29 +2,32 @@
 
 namespace Copromatic\MailgunAdminBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
 class WebhookController extends Controller {
 
     public function bounceAction(Request $request) {
-        dump($request);
+        dump($request);die;
     }
 
     public function deliverAction(Request $request) {
-        dump($request);
+        dump($request);die;
     }
 
     public function dropAction(Request $request) {
-        dump($request);
+        dump($request);die;
     }
 
     public function spamAction(Request $request) {
-        dump($request);
+        dump($request);die;
     }
 
     public function clickAction(Request $request) {
-        dump($request);
+        dump($request);die;
     }
 
     public function openAction(Request $request) {
-        dump($request);
+        dump($request);die;
     }
 }
